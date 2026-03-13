ecf37dce99320104ffa492261f1c1e54967a482c
Harmeet Walia <harmeetsinghwalia69@gmail.com>
2026-03-13 13:18:35 +0530

    Running code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+
 import { TooltipProvider } from "@/components/ui/tooltip";
+
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
-import About from "./pages/about";
-import Feedback from "./pages/feedback";
+import About from "@/pages/about";
+import Feedback from "@/pages/feedback";
 
 function Router() {
   return (
@@ -23,7 +24,6 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Toaster />
         <Router />
       </TooltipProvider>
     </QueryClientProvider>
